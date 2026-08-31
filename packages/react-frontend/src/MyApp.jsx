b6e36f022af89150e841a7c7b150381dacb27fa6
Diego Melgoza <drmelgoza@icloud.com>
2026-08-31 09:15:00 -0700

    completed steps 1-7

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,11 +1,57 @@
 // src/MyApp.jsx
-import React from "react";
+import React, {useState} from "react";
 import Table from "./Table";
 
+const characters = [
+    {
+        name: "Charlie",
+        job: "Janitor"
+    },
+    {
+        name: "Mac",
+        job: "Bouncer"
+    },
+    {
+        name: "Dee",
+        job: "Aspiring actress"
+    },
+    {
+        name: "Dennis",
+        job: "Bartender"
+    }
+];
+
 function MyApp() {
+    const [characters, setCharacters] = useState([
+        {
+            name: "Charlie",
+            job: "Janitor"
+        },
+        {
+            name: "Mac",
+            job: "Bouncer"
+        },
+        {
+            name: "Dee",
+            job: "Aspiring actress"
+        },
+        {
+            name: "Dennis",
+            job: "Bartender"
+        }
+    ]);
+
+    function removeOneCharacter(index) {
+        const updated = characters.filter((character, i) => {
+            return i !== index;
+        })
+        setCharacters(updated);
+    }
     return (
         <div className="container">
-            <Table />
+            <Table characterData={characters}
+                   removeCharacter={removeOneCharacter}
+            />
         </div>
     );
 }
--- packages/react-frontend/src/Table.jsx
+++ packages/react-frontend/src/Table.jsx
@@ -5,40 +5,39 @@ function TableHeader() {
         <tr>
             <th>Name</th>
             <th>Job</th>
+            <th>Action</th>
         </tr>
         </thead>
     );
 }
 
-function TableBody() {
-    return (
-        <tbody>
-        <tr>
-            <td>Charlie</td>
-            <td>Janitor</td>
-        </tr>
-        <tr>
-            <td>Mac</td>
-            <td>Bouncer</td>
-        </tr>
-        <tr>
-            <td>Dee</td>
-            <td>Aspiring Actress</td>
-        </tr>
-        <tr>
-            <td>Dennis</td>
-            <td>Actor</td>
-        </tr>
-        </tbody>
+function TableBody(props) {
+    const rows = props.characterData.map((row, index) => {
+        return (
+            <tr key={index}>
+                <td>{row.name}</td>
+                <td>{row.job}</td>
+                <td>
+                    <button onClick={() => props.removeCharacter(index)}>
+                        Delete
+                    </button>
+                </td>
+            </tr>
+        );
+    }
     );
-
+    return (
+        <tbody>{rows}</tbody>
+    );
 }
 
-function Table() {
+function Table(props) {
     return (
         <table>
             <TableHeader />
-            <TableBody />
+            <TableBody characterData={props.characterData}
+                       removeCharacter={props.removeCharacter}
+            />
         </table>
     );
 }
